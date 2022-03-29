3257bd08b86b3a1e5149a6d155990ab98918595c
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2022-03-29 16:21:52 -0300

    feat: :sparkles: Implementa todas as formas de busca sql com typeorm

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -1,30 +1,34 @@
-import { getRepository, Repository } from 'typeorm';
+import { getRepository, Repository } from "typeorm";
 
-import { IFindUserWithGamesDTO, IFindUserByFullNameDTO } from '../../dtos';
-import { User } from '../../entities/User';
-import { IUsersRepository } from '../IUsersRepository';
+import { IFindUserWithGamesDTO, IFindUserByFullNameDTO } from "../../dtos";
+import { User } from "../../entities/User";
+import { IUsersRepository } from "../IUsersRepository";
 
 export class UsersRepository implements IUsersRepository {
-  private repository: Repository<User>;
+    private repository: Repository<User>;
 
-  constructor() {
-    this.repository = getRepository(User);
-  }
+    constructor() {
+        this.repository = getRepository(User);
+    }
 
-  async findUserWithGamesById({
-    user_id,
-  }: IFindUserWithGamesDTO): Promise<User> {
-    // Complete usando ORM
-  }
+    async findUserWithGamesById({ user_id }: IFindUserWithGamesDTO): Promise<User> {
+        const user = await this.repository.findOne(user_id, { relations: ["games"] });
 
-  async findAllUsersOrderedByFirstName(): Promise<User[]> {
-    return this.repository.query(); // Complete usando raw query
-  }
+        if (user) {
+            return user;
+        } else {
+            throw new Error("User not found");
+        }
+    }
 
-  async findUserByFullName({
-    first_name,
-    last_name,
-  }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
-    return this.repository.query(); // Complete usando raw query
-  }
+    async findAllUsersOrderedByFirstName(): Promise<User[]> {
+        return this.repository.query("SELECT * FROM users order by first_name"); // Complete usando raw query
+    }
+
+    async findUserByFullName({ first_name, last_name }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
+        return this.repository.query(`SELECT * FROM users WHERE first_name ILIKE $1 AND last_name ILIKE $2`, [
+            first_name,
+            last_name,
+        ]);
+    }
 }
